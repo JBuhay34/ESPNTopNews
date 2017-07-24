b5214f5a312875ba73660411e96cf5d8c1c9b9f1
JBuhay <jbuhay3@gmail.com>
2017-07-24 16:14:56 -0700

    Got my app to connect to the internet and get the JSON string, still need to parse it and make it into articles

--- app/src/main/java/com/example/justinbuhay/espntopnews/MainActivity.java
+++ app/src/main/java/com/example/justinbuhay/espntopnews/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.justinbuhay.espntopnews;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.ViewGroup;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     private ESPNNewsRecyclerViewAdapter mAdapter;
 
@@ -26,5 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+        // Test Run
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        try {
+            myAsyncTask.execute(new URL(NetworkUtils.api_String));
+        } catch (MalformedURLException e){
+            Log.e(this.getClass().getSimpleName(), "Unable to make URL");
+        }
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/justinbuhay/espntopnews/NetworkUtils.java
@@ -0,0 +1,96 @@
+package com.example.justinbuhay.espntopnews;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by justinbuhay on 7/24/17.
+ */
+
+public class NetworkUtils {
+
+    private static String LOG_TAG = "NetworkUtils";
+
+    public static String api_String = "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=c7fb926751964ccc90f758f027682a2f";
+
+    public static String fetchArticleData(){
+
+        String jsonParsed = "";
+
+        URL newURL = convertToURL(api_String);
+
+        if(newURL != null){
+
+            Log.i(LOG_TAG, "" + newURL);
+            jsonParsed = makeHTTPURLConnection(newURL);
+
+
+        }
+
+        return jsonParsed;
+
+
+
+    }
+
+
+    private static String makeHTTPURLConnection(URL url){
+
+        HttpURLConnection connection = null;
+        String jsonParsed = null;
+
+        try{
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            jsonParsed = makeStreamConverter(connection);
+
+
+        } catch(IOException e){
+            Log.e(LOG_TAG, "Unable to make connection to URL");
+        } finally {
+            connection.disconnect();
+        }
+
+        return jsonParsed;
+
+
+    }
+
+    private static String makeStreamConverter(HttpURLConnection connection) throws IOException{
+        InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+        String tempString;
+
+        BufferedReader reader = new BufferedReader(streamReader);
+        StringBuilder builder = new StringBuilder();
+
+        while((tempString = reader.readLine()) != null){
+            builder.append(tempString);
+        }
+
+        return builder.toString();
+
+    }
+
+
+
+    private static URL convertToURL(String api_String){
+
+        try {
+            URL newURL = new URL(api_String);
+            return newURL;
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Unable to convert the string to a URL");
+        }
+        return null;
+
+    }
+
+}
